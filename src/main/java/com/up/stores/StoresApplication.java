58f6858e9010e6eb2773ff2307a62c61503c506a
九键 <1390766763@qq.com>
2021-11-26 23:25:35 +0800

    上传个人图片

--- src/main/java/com/up/stores/StoresApplication.java
+++ src/main/java/com/up/stores/StoresApplication.java
@@ -3,8 +3,16 @@ package com.up.stores;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.unit.DataSize;
+import org.springframework.util.unit.DataUnit;
+
+import javax.servlet.MultipartConfigElement;
 
 
+@Configuration
 @SpringBootApplication
 @MapperScan("com.up.stores.mapper")
 //配置mapper接口位置
@@ -13,4 +21,14 @@ public class StoresApplication {
         SpringApplication.run(StoresApplication.class, args);
     }
 
+    @Bean
+    public MultipartConfigElement getMultipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        // DataSize dataSize = DataSize.ofMegabytes(10);
+        // 设置文件最大10M，DataUnit提供5中类型B,KB,MB,GB,TB
+        factory.setMaxFileSize(DataSize.of(10, DataUnit.MEGABYTES));
+        factory.setMaxRequestSize(DataSize.of(15, DataUnit.MEGABYTES));
+        // 设置总上传数据总大小10M
+        return factory.createMultipartConfig();
+    }
 }
